29273d5b4f25c80f0eca29d0d1eb5f7380bc0400
wh3d0 <dasika74@gmail.com>
2026-02-05 10:21:22 +0700

    Delete ru/clouddonate/cloudpaymentslegacy directory

--- ru/clouddonate/cloudpaymentslegacy/messengers/api/ConnectException.java
+++
@@ -1,7 +0,0 @@
-package ru.clouddonate.cloudpaymentslegacy.messengers.api;
-
-public class ConnectException extends RuntimeException {
-   public ConnectException(String message) {
-      super(message);
-   }
-}
--- ru/clouddonate/cloudpaymentslegacy/messengers/api/MessengerService.java
+++
@@ -1,9 +0,0 @@
-package ru.clouddonate.cloudpaymentslegacy.messengers.api;
-
-public interface MessengerService {
-   void connect() throws ConnectException;
-
-   void disconnect();
-
-   void sendMessage(String var1);
-}
